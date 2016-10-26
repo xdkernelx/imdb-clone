7a04ac49dab365022a92a163c1214507d1f71b4c
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-26 16:23:41 -0700

    Change the validation of Devise email once again. Should be ok for regular clients and not Enterprise emails.

--- filmster/config/initializers/devise.rb
+++ filmster/config/initializers/devise.rb
@@ -160,7 +160,7 @@ Devise.setup do |config|
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
   # DEFAULT: /\A[^@\s]+@[^@\s]+\z/
-  config.email_regexp = /^[\d\w_]+\.?[\d\w_]*@[\d\w]+\.?[\d\w]*\.[\d\w]{2,}\.?[\d\w]{2,}$/
+  config.email_regexp = /^[^@\s]+@[\d\w]+\.?[\d\w]*\.?[\d\w]*\.+[\d\w]*$/
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
