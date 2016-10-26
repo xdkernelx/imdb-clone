6bceca149cfb0fac121c630ab9fbc4e2d6699204
Ellie Dori <email": null>
2016-10-26 16:37:13 -0700

    Add further associations and white space

--- filmster/app/models/comment.rb
+++ filmster/app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :review
   belongs_to :commenter, class_name: 'User', foreign_key: 'commenter_id'
+  has_one :film, through: :review
+
+
+
+
+
+
+
 end
--- filmster/app/models/film.rb
+++ filmster/app/models/film.rb
@@ -2,4 +2,12 @@ class Film < ApplicationRecord
   belongs_to :category
   has_many :reviews
   has_many :reviewers, through: :reviews, foreign_key: 'reviewer_id'
+
+
+
+
+
+
+
+
 end
