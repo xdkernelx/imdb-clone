94e4a9383f14e743b8bde93cc2f542a58b7e43a3
Mal Wood <mallorywood413@gmail.com>
2016-10-27 12:27:04 -0700

    Revert "Reviews categories"

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ApplicationRecord
-  belongs_to :review
-  belongs_to :commenter, class_name: 'User', foreign_key: 'commenter_id'
-  has_one :film, through: :review
-
-  validates :commenter_id, :review_id, { presence: true }
-
-end
--- app/models/review.rb
+++
@@ -1,9 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :reviewer, class_name: 'User', foreign_key: 'reviewer_id'
-  belongs_to :film
-  has_one :category, through: :film
-
-  validates :title, :body, :rating, :reviewer_id, :film_id, { presence: true }
-  validates :rating, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
-
-end
