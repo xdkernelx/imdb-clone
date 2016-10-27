186fa992e9552a8b7aabd0ff66d587108f43e5e0
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-26 17:24:02 -0700

    Add presence validation for several of our fields of the User model.

--- filmster/app/models/user.rb
+++ filmster/app/models/user.rb
@@ -6,10 +6,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-
-
-
-
-
+  validates :first_name, :last_name, :email, { presence: true }
 
 end
