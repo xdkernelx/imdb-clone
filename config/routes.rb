c2c04d4cf77c364b2d9dfd7e9c1063613e543e02
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-27 14:26:12 -0700

    TEMP - switching branches.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    get "users/sign_in" => "films#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
+
   devise_for :users
   root "films#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
