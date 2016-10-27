2050a1e751608ec3a835e0919e2b36d754c0242e
Nestor Alvarez <grimreaper1169@gmail.com>
2016-10-27 15:16:20 -0700

    Add a route resource to comments.:

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   root "films#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :categories
+  resources :comments
   resources :reviews, only: [:index]
   resources :films do
     resources :reviews
